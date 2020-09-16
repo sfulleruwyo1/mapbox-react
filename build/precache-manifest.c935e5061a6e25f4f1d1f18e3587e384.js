ce70faffa8bf55fc351050e24859e0d853759ecd
sfulleruwyo <sfu234@uky.edu>
2020-09-16 14:39:14 -0600

    Added react build data
    Added completed react build

---
+++ build/precache-manifest.c935e5061a6e25f4f1d1f18e3587e384.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c4355e5dfbcf8f5498181d172a1107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09111e8cb4f741aa3317",
+    "url": "/static/js/2.80b0e790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80b0e790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a325c6d50e8e88921e63",
+    "url": "/static/js/main.e8e2dd72.chunk.js"
+  },
+  {
+    "revision": "c6526294efe35cb1d653",
+    "url": "/static/js/runtime-main.7d5ced05.js"
+  }
+]);
